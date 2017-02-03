5389959e442110c4dbec1e4e9025a34d10e3e6ed
ajaybhatia <prof.ajaybhatia@gmail.com>
2017-02-03 13:32:48 +0530

    Helper and Login screen added

---
+++ src/com/ajaybhatia/utilities/Helper.java
@@ -0,0 +1,48 @@
+package com.ajaybhatia.utilities;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.ajaybhatia.entities.controllers.UserJpaController;
+import java.awt.Component;
+import javax.persistence.Persistence;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ajay
+ */
+public class Helper {
+
+    public static UserJpaController getUserControllerInstance() {
+        return new UserJpaController(
+            Persistence.createEntityManagerFactory("Gulati-GarmentsPU")
+        );
+    }
+        
+    public static void centerScreen(JFrame frame) {
+        frame.setLocationRelativeTo(null);
+    }
+    
+    public static void showSuccessMessage(Component component, String message) {
+        JOptionPane.showMessageDialog(component, message, "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    public static void showErrorMessage(Component component, String message) {
+        JOptionPane.showMessageDialog(component, message, "Message", JOptionPane.ERROR_MESSAGE);
+    }
+    
+    public static void closeAndOpen(JFrame toClose, JFrame toOpen) {
+        toClose.dispose();
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                toOpen.setVisible(true);
+            }
+        });
+    }
+}
