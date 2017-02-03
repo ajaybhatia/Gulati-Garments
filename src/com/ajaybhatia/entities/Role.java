dc4485b23d203865cf45be504e292ca0b72f0205
ajaybhatia <prof.ajaybhatia@gmail.com>
2017-02-03 13:43:27 +0530

    ID should generate in sequence rather than auto stratergy

--- src/com/ajaybhatia/entities/Role.java
+++ src/com/ajaybhatia/entities/Role.java
@@ -20,7 +20,7 @@ public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
 
--- src/com/ajaybhatia/entities/User.java
+++ src/com/ajaybhatia/entities/User.java
@@ -21,7 +21,7 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String username;
     private String password;
